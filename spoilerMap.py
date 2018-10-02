b1791685bb7f4a89e69dbd2d0caa5a5d4ad74140
inSLaine <43711110+inSLaine@users.noreply.github.com>
2018-10-02 10:53:36 -0500

    Add files via upload
    Added zoom and pan

--- spoilerMap.py
+++ spoilerMap.py
@@ -1,29 +1,36 @@
+import kivy
+import tkinter
+import time
+import sys
+import os
+import configparser
+import re
 from kivy.config import Config
 from kivy.core.window import Window
-import kivy
 
-kivy.config.Config.set('graphics','resizable', 1)
-kivy.config.Config.set('graphics','borderless', 0)
-Window.size = (700, 700)
-Config.write()
+# kivy.config.Config.set('graphics','resizable', 1)
+# kivy.config.Config.set('graphics','borderless', 0)
+# Window.size = (700, 700)
+# Config.write()
 
+from kivy.uix.scatter import Scatter
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
-from  kivy.uix.filechooser import FileChooserIconView
+from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from kivy.uix.image import Image
-import time
-import os
+
+if getattr(sys, 'frozen', False):
+# we are running in a |PyInstaller| bundle
+	base_path = sys._MEIPASS
+
+
 from kivy.lang import Builder
-import tkinter
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-import configparser
-import re
-
 
 
 mapWidth = 4000
@@ -58,60 +65,102 @@ bottleItems = ['BottleWithGreenPotion','BottleWithFairy','BottleWithRedPotion','
 #Main Window / Canvas
 class screenMan(ScreenManager):
 
-	def setBottles(self, x):
-		if(x[1] in bottleItems):
-			x[1] = 'Bottle{}'.format(countRepeatItems['bottleCount'])
+	Window.size = (600,600)
+	
+	def on_touch_up(self, touch):
+		if touch.is_mouse_scrolling:
+			if touch.button == 'scrolldown':
+				if(self.ids.lightScatter.scale < 4):
+					self.ids.lightScatter.scale = self.ids.lightScatter.scale * 1.2
+					self.ids.darkScatter.scale = self.ids.darkScatter.scale * 1.2
+				pass
+			elif touch.button == 'scrollup':
+				if(self.ids.lightScatter.scale > 1):
+					self.ids.lightScatter.scale = self.ids.lightScatter.scale * 0.8
+					self.ids.darkScatter.scale = self.ids.darkScatter.scale * 0.8
+				pass
+				
+	def zoomMap(self):
+		pass
+		# if touch.is_mouse_scrolling:
+			# if touch.button == 'scrolldown':
+				# print('down')
+				# ## zoom in
+				# if self.scale < 10:
+					# self.scale = self.scale * 1.1
+
+		# elif touch.button == 'scrollup':
+			# ## zoom out
+			# print('up')
+			# if self.scale > 1:
+				# self.scale = self.scale * 0.8
+
+	def resource_path(self, relative_path):
+		""" Get absolute path to resource, works for dev and for PyInstaller """
+		try:
+			# PyInstaller creates a temp folder and stores path in _MEIPASS
+			base_path = sys._MEIPASS
+		except Exception:
+			base_path = os.path.abspath(".")
+
+		return os.path.join(base_path, relative_path)
+
+	def setBottles(self, y):
+		if(y in bottleItems):
+			y = 'Bottle{}'.format(countRepeatItems['bottleCount'])
 			countRepeatItems['bottleCount'] += 1
-			return x
+			return y
 		else:
-			return x
+			return y
 			
-	def setSwords(self, x):
-		if(x[1] == "ProgressiveSword"):
-			x[1] = 'ProgressiveSword{}'.format(countRepeatItems['progSword'])
+	def setSwords(self, y):
+		if(y == "ProgressiveSword"):
+			y = 'ProgressiveSword{}'.format(countRepeatItems['progSword'])
 			countRepeatItems['progSword'] += 1
-			return x
+			return y
 		else:
-			return x
+			return y
 			
-	def setGloves(self, x):
-		if(x[1] == "ProgressiveGlove"):
-			x[1] = 'ProgressiveGlove{}'.format(countRepeatItems['progGlove'])
+	def setGloves(self, y):
+		if(y == "ProgressiveGlove"):
+			y = 'ProgressiveGlove{}'.format(countRepeatItems['progGlove'])
 			countRepeatItems['progGlove'] += 1
-			return x
+			return y
 		else:
-			return x
+			return y
 	
 	def on_size(self, *args):
-		Window.size = (Window.height, Window.height)
-		Window.size = (Window.width, Window.width)
+		w = Window.width
+		h = Window.height
+		Window.size = (w, h)
 		
 	def lightWorldLocations(self):
-		f = open('itemLocations/lightWorldLocations.txt', 'r')
+		f = open((self.resource_path('itemLocations/lightWorldLocations.txt')), 'r')
 		for line in f:
 			x = line.split(':')
 			lightWorldLocations[x[0]] = (int(x[1])/mapWidth, 1 - int(x[2])/mapHeight)
 			
 	def darkWorldLocations(self):
-		f = open('itemLocations/darkWorldLocations.txt', 'r')
+		f = open((self.resource_path('itemLocations/darkWorldLocations.txt')), 'r')
 		for line in f:
 			x = line.split(':')
 			darkWorldLocations[x[0]] = (int(x[1])/mapWidth, 1 - int(x[2])/(mapHeight))
 		
 	def placeWorldItems(self):
 		for each in requiredItems:
-			x = Image(source='artAssets/{}.png'.format(each))
+			x = Image(source=self.resource_path('artAssets/{}.png'.format(each)))
 			x.allow_stretch = True
 			x.keep_ratio = True
 			x.size_hint = (.03,.03)
 			if(allWorldItems[each] in lightWorldLocations):
 				x.pos_hint = {'center_x':lightWorldLocations[allWorldItems[each]][0],'center_y':lightWorldLocations[allWorldItems[each]][1]}
+				x.text = '{}:{}'.format(each, allWorldItems[each])
 				x.reload()
-				self.ids.lightWorldPage.add_widget(x,1)
+				self.ids.lightScatter.add_widget(x,0)
 			elif(allWorldItems[each] in darkWorldLocations):
 				x.pos_hint = {'center_x':darkWorldLocations[allWorldItems[each]][0],'center_y':darkWorldLocations[allWorldItems[each]][1]}
 				x.reload()
-				self.ids.darkWorldPage.add_widget(x,1)
+				self.ids.darkScatter.add_widget(x,0)
 			else:
 				pass
 
@@ -121,7 +170,7 @@ class screenMan(ScreenManager):
 		f = open(filename, 'r')
 		for line in f:
 			cleanLine = line.strip()
-			noSpace = re.sub('\s+', '', cleanLine)
+			noSpace = re.sub('', '', cleanLine)
 			noQuote = re.sub('"', '', noSpace)
 			noOBrack = re.sub('{', '', noQuote)
 			noCBrack = re.sub('}', '', noOBrack)
@@ -129,10 +178,11 @@ class screenMan(ScreenManager):
 			purgedLine = re.sub(',', '', noParens)
 			if ":" in purgedLine:
 				x = purgedLine.split(':')
-				x = self.setBottles(x)
-				x = self.setSwords(x)
-				x = self.setGloves(x)
-				allWorldItems[x[1]] = x[0]
+				y = re.sub('\s+', '', x[1])
+				y = self.setBottles(y)
+				y = self.setSwords(y)
+				y = self.setGloves(y)	
+				allWorldItems[y] = x[0]
 		self.lightWorldLocations()
 		self.darkWorldLocations()
 		self.placeWorldItems()
@@ -142,8 +192,11 @@ class screenMan(ScreenManager):
 
 class spoilerMap(App):
 	def build(self):
+	
+
+
 		
 		return screenMan()
 		
 if __name__ == '__main__':
-    spoilerMap().run()
+	spoilerMap().run()
